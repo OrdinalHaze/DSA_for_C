c64a22e4ec76c2bb25aed8d2a321671770f6f554
Sebin Geo <sebingeo2001@gmail.com>
2025-07-03 19:22:43 +0530

    Traversal and insertion code for C

---
+++ array/insertion.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void main(){
+
+    int a[10]={1,4,5,2,34,67,34,34};
+    int loc;
+    int n= 8;
+    int size = 10;
+    int item;
+
+
+    printf("Enter the index to be inserted \n");
+    scanf("%d", &loc);
+
+    printf("Enter the iteam to insert\n");
+    scanf("%d",&item);
+
+    if(n==size){
+        printf("Its full no new stuff can be inserted\n");
+
+    }else{
+
+        for(int i = n-1;i>=loc;i--){
+            a[i+1]=a[i];
+            
+        }
+        a[loc] = item;
+        n=n+1;
+
+    }
+
+    for(int i =0;i<10;i++){
+
+        printf("%d\t",a[i]);
+    }
+
+
+}
---
+++ array/traversal.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+
+    int a[15]={1,2,3,4,5};
+    for(int i = 0;i<5;i++){
+
+        printf("%d\n",a[i]);
+    }
+
+}
